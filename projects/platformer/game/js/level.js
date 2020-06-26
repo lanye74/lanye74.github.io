f323fc159c4337e080af5fa1ccbeea8823eaef42
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2020-06-26 16:52:30 -0400

    platformer getting a move on; list to self: clouds, background first

--- projects/platformer/game/js/level.js
+++ projects/platformer/game/js/level.js
@@ -1,20 +1,25 @@
-let level = maps[0];
-ctx.fillStyle = "#ffffff";
+function renderLevel(level) {
 
-function drawLevel() {
-	
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 	for(let y = 0; y < level.length; y++) {
 		for(let x = 0; x < level[y].length; x++) {
 			let tile = level[y][x];
 
-			if(tile === 0) {continue;}
-			if(tile === 1) {draw(x, y);}
+			if(tile === 0) {draw(tiles.sky, x, y);}
+			if(tile === 1) {draw(tiles.dirt, x, y);}
 		}
 	}
 }
 
-function draw(x, y) {
-	ctx.fillRect(x * tile.width, y * tile.height, tile.width, tile.height);
+function draw(type, x, y) {
+	if(typeof type === "string") {
+		ctx.fillStyle = type;
+		ctx.fillRect(x * tile.width, y * tile.height, tile.width, tile.height);
+	} else if(typeof type === "object") { // HTMLImageElement
+		ctx.drawImage(type, x * tile.width, y * tile.height);
+	} else {
+		console.error(`Error drawing tile at ${x}, ${y}: ${type} has typeof ${typeof type} and is invalid.`)
+	}
 }
--- projects/platformer/game/js/main.js
+++ projects/platformer/game/js/main.js
@@ -1,21 +1,54 @@
 const c = document.getElementById("canv");
 const ctx = c.getContext("2d");
 
+
 const canvas = {
 	width: 1280,
 	height: 720
 };
 
 const tile = {
-	width: 20,
-	height: 20
+	width: 40,
+	height: 40
+};
+
+const tiles = {
+	dirt: undefined,
+	sky: "#61c8e7"
 };
 
 const fps = 60;
 
 
+let skipFrame = false;
+
+
+ctx.fillStyle = "#ffffff";
+
+
+let currentLevel = 1;
 
 
 let mainLoop = setInterval(() => {
-	drawLevel();
-}, 1000 / fps); /* ugh code */
+	if((Object.keys(tiles).filter(key => !tiles[key] === undefined)).length === 0) // if no properties are null
+	{} else {skipFrame = true; console.log("skipping frame")}
+
+	if(!skipFrame) {
+		// updateLevel();
+		renderLevel(maps[currentLevel - 1]);
+
+		skipFrame = false;
+	}
+}, 1000 / fps);
+
+
+
+(() => { // define the tiles
+	let dirt = new Image(20, 20);
+
+	dirt.src = "../../../assets/platformer/dirt.png";
+
+	dirt.onload = () => {console.log('loaded')}
+
+	tiles.dirt = dirt;
+})();
