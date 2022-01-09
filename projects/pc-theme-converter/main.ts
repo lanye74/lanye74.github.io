c384b24c1d616ff2bc803aaa35eb87f914007e6c
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2022-01-09 14:16:13 -0500

    Update classes json

--- projects/pc-theme-converter/main.ts
+++ projects/pc-theme-converter/main.ts
@@ -2,6 +2,16 @@ let text: string = "";
 let url: string;
 let fileName: string = "";
 
+let jsonData: Object;
+
+
+window.onload = async () => {
+	jsonData = await (await fetch("./classes.json")).json();
+
+	// probably make code blocking
+}
+
+
 
 async function processFile(file: File): Promise<void> {
 	const reader = new FileReader();
@@ -26,26 +36,25 @@ async function processCSS(event: unknown): Promise<void> {
 	text = event.target.result;
 
 
-	const jsonData = await (await fetch("./classes.json")).json();
-
-
 	for(const [key, value] of Object.entries(jsonData)) {
 		text = text.replaceAll(key, <string>value);
 	}
 
 
-	// navigator.clipboard.writeText(text);
-
 	document.getElementById("saveOptions")!.classList.add("show");
 }
 
+
+
 async function copyToClipboard(): Promise<void> {
-	navigator.clipboard.writeText(text);
+	await navigator.clipboard.writeText(text);
 
 	document.getElementById("saveOptions")!.classList.remove("show");
 }
 
-async function downloadAsFile(): Promise<void> {
+
+
+function downloadAsFile(): void {
 	const blob = new Blob([text], {
 		type: "text/css"
 	});
