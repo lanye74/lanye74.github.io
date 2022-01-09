667484dc9558923a06c4c1c910d64569a0407f9e
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2022-01-09 13:59:43 -0500

    Update with new download methods

--- projects/pc-theme-converter/main.js
+++ projects/pc-theme-converter/main.js
@@ -1,16 +1,40 @@
 "use strict";
+let text = "";
+let url;
+let fileName = "";
 async function processFile(file) {
     const reader = new FileReader();
     reader.onload = processCSS;
+    fileName = file.name;
     reader.readAsText(file);
 }
 async function processCSS(event) {
+    if (url) {
+        URL.revokeObjectURL(url);
+    }
     // @ts-ignore no idea what the event type is sjfdjlfdsjlkfdsjlkfsdljkfsdjklsfdjlk
-    let text = event.target.result;
+    text = event.target.result;
     const jsonData = await (await fetch("./classes.json")).json();
     for (const [key, value] of Object.entries(jsonData)) {
         text = text.replaceAll(key, value);
     }
+    // navigator.clipboard.writeText(text);
+    document.getElementById("saveOptions").classList.add("show");
+}
+async function copyToClipboard() {
     navigator.clipboard.writeText(text);
-    document.getElementById("copied").classList.add("appear");
+    document.getElementById("saveOptions").classList.remove("show");
+}
+async function downloadAsFile() {
+    const blob = new Blob([text], {
+        type: "text/css"
+    });
+    url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = fileName;
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    document.getElementById("saveOptions").classList.remove("show");
 }
--- projects/pc-theme-converter/main.ts
+++ projects/pc-theme-converter/main.ts
@@ -1,7 +1,14 @@
+let text: string = "";
+let url: string;
+let fileName: string = "";
+
+
 async function processFile(file: File): Promise<void> {
 	const reader = new FileReader();
 
 	reader.onload = processCSS;
+
+	fileName = file.name;
 
 
 	reader.readAsText(file);
@@ -10,18 +17,51 @@ async function processFile(file: File): Promise<void> {
 
 
 async function processCSS(event: unknown): Promise<void> {
+	if(url) {
+		URL.revokeObjectURL(url);
+	}
+
+
 	// @ts-ignore no idea what the event type is sjfdjlfdsjlkfdsjlkfsdljkfsdjklsfdjlk
-	let text = <string>event.target.result;
+	text = event.target.result;
 
 
 	const jsonData = await (await fetch("./classes.json")).json();
+
 
 	for(const [key, value] of Object.entries(jsonData)) {
 		text = text.replaceAll(key, <string>value);
 	}
 
 
+	// navigator.clipboard.writeText(text);
+
+	document.getElementById("saveOptions")!.classList.add("show");
+}
+
+async function copyToClipboard(): Promise<void> {
 	navigator.clipboard.writeText(text);
 
-	document.getElementById("copied")!.classList.add("appear");
+	document.getElementById("saveOptions")!.classList.remove("show");
+}
+
+async function downloadAsFile(): Promise<void> {
+	const blob = new Blob([text], {
+		type: "text/css"
+	});
+
+
+	url = URL.createObjectURL(blob);
+
+	const a = document.createElement("a");
+	a.download = fileName;
+	a.href = url;
+
+	document.body.appendChild(a);
+
+	a.click();
+
+	document.body.removeChild(a);
+
+	document.getElementById("saveOptions")!.classList.remove("show");
 }
